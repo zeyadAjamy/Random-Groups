7e6a38a3a65b1627282f6544d4ab831a4f42977b
zeyadAjamy <wezza2003@gmail.com>
2020-07-22 07:56:51 +0200

    RANDOM INIT.

---
+++ random.js
@@ -0,0 +1,19 @@
+/* C for Zeyad Ajamy -- 2020 --
+  How To use:
+  ------------
+   +  [YOur ITemS].random(Max Num in the group)
+*/
+Array.prototype.random = function(x){
+  let final  = [];
+  let totalRand =  
+    this.sort(_=>{
+        Math.random -0.5
+    })
+    for (let i =0; i<=totalRand.length/x; i++){
+      final.push(totalRand.splice(0, x))
+    }
+    if(totalRand.length != 0){
+      final.push(totalRand)
+    }
+    return final;
+};
